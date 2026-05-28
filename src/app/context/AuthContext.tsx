cf70035f815e25031dd0c5a4038d528f05add3ac
Brenno Luan <brennoluan8530@gmail.com>
2026-05-28 14:19:35 -0300

    Add loading state to AuthProvider for improved user experience during session retrieval

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -12,11 +12,15 @@ export const AuthContext = createContext<IAuthContext | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+    supabase.auth
+      .getSession()
+      .then(({ data: { session } }) => {
+        setSession(session);
+      })
+      .finally(() => setLoading(false));
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
@@ -44,7 +48,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <AuthContext.Provider value={{ session, logout, login }}>
-      {children}
+      {loading ? "carregando..." : children}
     </AuthContext.Provider>
   );
 };
